78771c900aa25748796dff9d2d22c6c5125fee94
Ishrat Jahan Rintu <155283857+Ishrat2413@users.noreply.github.com>
2025-11-29 17:36:52 +0600

    Create README.md

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
